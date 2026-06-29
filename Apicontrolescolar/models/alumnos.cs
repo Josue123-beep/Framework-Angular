a35a08b856b602aee6f2117da72c5b632ced7e0d
Josue123-beep <josueyaelrubiosanchez@gmail.com>
2026-06-29 11:40:08 -0600

    Proyecto completo actualizado

---
+++ Apicontrolescolar/models/alumnos.cs
@@ -0,0 +1,7 @@
+﻿public class Alumno
+{
+    public string Matricula { get; set; }
+    public int IdCarrera { get; set; }
+    public int IdDatosP { get; set; }
+    public bool Status { get; set; }
+}
